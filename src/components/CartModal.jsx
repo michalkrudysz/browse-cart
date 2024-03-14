259e288abd4e8df82ee86d93eecfc302763e716a
Michal <michal@drontrzebownisko.pl>
2024-03-14 12:32:40 +0100

    Added support for opening and closing the modal through state management in the parent component.

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
 import ReactDOM from "react-dom";
 import classes from "./CartModal.module.scss";
 
-export default function CartModal() {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
-
-  if (!isOpen) {
-    return null;
-  }
-
+export default function CartModal({ close }) {
   return ReactDOM.createPortal(
     <div className={classes["cart-modal"]}>
       <div className={classes["cart-modal-content"]}>
         <h2>Twój koszyk</h2>
-        <button onClick={closeModal}>Zamknij</button>
+        <button onClick={() => close(false)}>Zamknij</button>
       </div>
     </div>,
     document.getElementById("cart")
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,33 @@ import classes from "./Header.module.scss";
 import logo from "../../public/logo.png";
 import shoppingCart from "../assets/add-to-basket.png";
 import CartModal from "./CartModal";
+import { useState } from "react";
 
 export default function Header({ cartItemCount }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toogleCartModal(value) {
+    if (value === false) {
+      setIsOpen(false);
+    } else setIsOpen(true);
+  }
+
+  function renderCartModal() {
+    if (isOpen) {
+      return <CartModal close={toogleCartModal} />;
+    } else {
+      return null;
+    }
+  }
+
   return (
     <>
-      <CartModal />
+      {renderCartModal()}
       <header className={classes.header}>
         <div className={classes.logo}>
           <img src={logo} alt="Logo" />
         </div>
-        <div className={classes.cart}>
+        <div onClick={() => toogleCartModal(true)} className={classes.cart}>
           <img
             src={shoppingCart}
             alt="Ikona koszyka sklepowego"
