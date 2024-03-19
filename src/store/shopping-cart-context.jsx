f4379347701c1936f11585f70c9a4d66e3554254
Michal <michal@drontrzebownisko.pl>
2024-03-19 11:47:51 +0100

    Reverted to a previous commit version to restart useReducer from scratch for a deeper understanding of the logic.

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,4 +1,90 @@
-import { createContext, useReducer } from "react";
+import { createContext } from "react";
+import { useState } from "react";
+
+export const CartContext = createContext({
+  cart: [],
+  addItem: () => {},
+  add: () => {},
+  remove: () => {},
+});
+
+export default function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  function addItem(product) {
+    const existingProduct = cartItems.find((item) => item.id === product.id);
+
+    if (existingProduct) {
+      const updatedItems = cartItems.map((item) => {
+        if (item.id === product.id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          };
+        } else {
+          return item;
+        }
+      });
+
+      setCartItems(updatedItems);
+    } else {
+      const newItem = {
+        ...product,
+        quantity: 1,
+      };
+
+      setCartItems([...cartItems, newItem]);
+    }
+  }
+
+  function remove(id) {
+    setCartItems((currentItems) => {
+      const existingProduct = currentItems.find((item) => item.id === id);
+      if (!existingProduct) {
+        return currentItems;
+      }
+      if (existingProduct.quantity > 1) {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return {
+              ...item,
+              quantity: item.quantity - 1,
+            };
+          } else {
+            return item;
+          }
+        });
+      } else {
+        return currentItems.filter((item) => item.id !== id);
+      }
+    });
+  }
+  function add(id) {
+    setCartItems((currentItems) => {
+      return currentItems.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          };
+        } else {
+          return item;
+        }
+      });
+    });
+  }
+  const ctxValue = {
+    cart: cartItems,
+    addItem: addItem,
+    remove: remove,
+    add: add,
+  };
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
+"  do tego mamy dojść:"import { createContext, useReducer } from "react";
 
 export const CartContext = createContext();
 
